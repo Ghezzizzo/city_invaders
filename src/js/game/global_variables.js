a86af8eb6c62a79dabbbff9bab455da6c9de5e5e
Guido Ghezzi <ghezzizzo@gmail.com>
2022-02-03 13:49:41 +0100

    enemy damage bug fixxed

--- src/js/game/global_variables.js
+++ src/js/game/global_variables.js
@@ -52,7 +52,7 @@ export const health = [ 100 + valuesList[8] * 4, 50 + valuesList[8] * 4,100 + va
 export const projectilePower = [5 + valuesList[2] * 2, 15 + valuesList[7] * 3]; //
 
 //ENEMY
-export const enemyDamage = [5 + Math.floor(valuesList[14]/2), 0.2 + valuesList[5]/2 ]; //
+export const enemyDamage = [5 + Math.floor(valuesList[14]/2), 1 + Math.floor(valuesList[5]/2) ]; //
 export const speeedEnemy = [Math.random() * 0.2 + valuesList[4] * 0.03,Math.random() * 0.2 + valuesList[15] * 0.1];//
 export const enemyHealth = [100 + valuesList[9]*100,100 + valuesList[13]*80];
 export const enemyDamageSpeed = 20 + Math.floor(valuesList[16]*0.1);
